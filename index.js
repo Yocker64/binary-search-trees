5b72931e2a4d59917ab347dd7298d13c257c07e2
Sandoval Mario <mdyoc74@gmail.com>
2025-06-27 16:35:26 +0900

    Add Tree and Node classes as well as their constructors and the pretty print to visualize the balanced tree

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right =  null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    array = mergeSort(removeRepeatedItems(array)); // Get rid of repeated items and ordering
+    let center = Math.floor(array.length/2)
+    let root = new Node(array[center]);
+    if (center > 0) {
+      root.left =new Tree(array.slice(0,center));
+    }
+    if (center<array.length) {
+    root.right =new Tree(array.slice(center+1,array.length));
+      
+    }
+    return root;
+  }
+}
 
 
 
 
+// Trim the array for not repeated elements and more easy handling 
 function removeRepeatedItems(array) {
   let alreadyUsed = []
 
@@ -21,9 +45,9 @@ function mergeSort(array) {
     return array;
   }
 
-  const middle = Math.floor(array.length / 2); // Use floor for consistency
-  const leftHalf = array.slice(0, middle);     // Non-modifying slice
-  const rightHalf = array.slice(middle);      // Non-modifying slice
+  const middle = Math.floor(array.length / 2);
+  const leftHalf = array.slice(0, middle);     
+  const rightHalf = array.slice(middle);     
 
   const sortedLeft = mergeSort(leftHalf);
   const sortedRight = mergeSort(rightHalf);
@@ -52,3 +76,16 @@ function orderOrderedArrays(array1, array2) {
   return orderedArraysOrdered
 }
 
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
